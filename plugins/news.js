668455c96d11474aa3ad3e8ef55379a1419c075f
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-05-05 22:42:14 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -41,3 +41,45 @@ async (conn, mek, m, { from, reply }) => {
         reply("Could not fetch news. Please try again later.");
     }
 });
+
+/////////////////////////////////
+
+cmd({
+    pattern: "hiru",
+    react: "⭐",
+    desc: "news",
+    category: "news",
+    use: ".hiru",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        // Fetching JSON data from the API endpoint
+        const response = await fetchJson(`https://mr-manul-ofc-apis.vercel.app/api/hiru-news?apikey=Manul-Official-Key-3467`);
+            // Extracting necessary fields from the JSON response
+            const title = response.data.title;
+            const date = response.data.date;
+            const desc = response.data.desc;
+            const link = response.data.link;
+            const image = response.data.img;
+            const createdBy = response.createdBy;
+            
+            // Craft the message to send to the user
+            const message = `
+> 🥷𝗗𝗔𝗥𝗞 𝗡𝗘𝗥𝗢 𝗛𝗜𝗥𝗨 𝗡𝗘𝗪𝗦📃
+
+*𝗧𝗶𝘁𝗹𝗲:* ${title}
+*𝗗𝗔𝗧𝗘:* ${date}
+*𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗧𝗜𝗢𝗡:* ${desc}
+*𝗥𝗘𝗔𝗗 𝗠𝗢𝗥𝗘:* ${link}
+
+> 🥷ᴘᴏᴡᴇʀᴅ ʙʏ ᴄʏʙᴇʀ ʏᴀᴋᴜᴢᴀ ᴛᴇᴀᴍ💀
+            `;
+
+            // Sending the message along with the image
+            await conn.sendMessage(from, { image: { url: image }, caption: message }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`Error: ${e}`);
+    }
+});
